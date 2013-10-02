56163711594982dc91a54c13666382dd8a51361e
Samuel Kindler <sam@flamov.com>
2013-10-02 16:08:29 +0800

    Added Episode #102 (int)

---
+++ episodes/102/index.php
@@ -0,0 +1,48 @@
+<?php $frontpage=false; $episodepage=true; $pagetitle=$headertitle="Episode #102: Three Life Therapy" ?>
+	
+	<?php include "../../lib/header.php"; ?>
+	
+	<div id="wrapper">
+		
+		<span class="section">
+
+			<h2>Description</h2>
+
+			<p>Feeling down because of Half-Life 3? Do not worry, we have your back, we are all going to make it. Topics include: GTA, Amnesia, Saints Row, Half-Life and Steam.</p>
+
+		</span>
+
+		<span class="section">
+
+			<h2>Listen</h2>
+
+			<div id="episode_audio_player">
+
+				<audio id="episode_audio"><source src="http://www.thesteamcast.com/episodes/101/steamcast_episode101.mp3" type="audio/mp3"></audio>
+
+				<div id="episode_audio_toggle"></div>
+
+				<div id="episode_audio_time"><span>00:00:00</span></div>
+
+				<div id="episode_audio_progress_wrapper">
+
+					<div id="episode_audio_progress"></div>
+
+				</div>
+
+			</div>
+
+			<ul style="clear:both;">
+
+				<li><a href="http://www.thesteamcast.com/episodes/101/steamcast_episode101.mp3">Direct MP3 Download</a></li>
+				<li><a href="http://www.thesteamcast.com/steamcast_feed.xml">Podcast RSS Feed (M4A)</a></li>
+				<li><a href="http://www.thesteamcast.com/steamcast_feed_mp3.xml">Podcast RSS Feed (MP3)</a></li>
+				<li><a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=320594165">iTunes Subscription</a></li>
+
+			</ul>
+
+		</span>
+		
+	</div>
+	
+<?php include "../../lib/footer.php"; ?>
