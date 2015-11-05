6e6d9c4355672af0138f9ccce4e9cf7419ca4b1f
Samuel Kindler <sam@flamov.com>
2015-11-05 18:03:26 +0800

    Added about page and updated CSS

---
+++ lib/aboutPage.php
@@ -0,0 +1,40 @@
+<?php
+
+	require_once "common.php";
+	require_once "header.php";
+
+?>
+
+
+
+<h1>About Steamchat</h1>
+
+<p>Steamchat is a fan-made podcast that hosts discussions about Valve, Steam, digital distribution, PC games and other related subjects such as the art and design of video games and other topical video game related subjects.</p>
+
+<p>Steamchat was created in early June of 2009, born from the controversy of the Left 4 Dead 2 announcement at E3 of the same year. Since then, it has been maintained by the three hosts; Sam, Saurabh, and Brad. Steamchat has enjoyed considerable success in that time. In August of 2009, Steamchat hosted its <a href="<?php echo $hostLocation; ?>episodes/9/">first exclusive interview with Gabe Newell</a>, co-founder and managing director of Valve Software. Staying true to its aim of supporting the community, Steamchat allowed members of the community the opportunity to forward their own questions and concerns to Newell, resulting in an entirely community-produced interview. Since then, Steamchat has published a further six community-based interviews (including <a href="<?php echo $hostLocation; ?>episodes/47/">another with Newell</a>, <a href="<?php echo $hostLocation; ?>snacks/2/">Harry S. Robins</a>, <a href="<?php echo $hostLocation; ?>pages/portal2party/">Marc Laidlaw</a>, <a href="<?php echo $hostLocation; ?>pages/portal2party/">Al&eacute;sia Glidewell</a>, <a href="<?php echo $hostLocation; ?>pages/portal2party/">Ellen McLain</a>, <a href="<?php echo $hostLocation; ?>snacks/3/">Jonathan Coulton</a> and the <a href="<?php echo $hostLocation; ?>snacks/4/">Black Mesa mod team</a>).</p>
+
+<p>As a community-founded podcast, Steamchat has always placed high importance on community participation, of which members are welcome to join the hosts and share their thoughts and opinions on specific topics. This gave the community - everyday gamers - the opportunity to voice their opinions and concerns.</p>
+
+<p>Steamchat temporarily ended in May 8th 2012 due to internal troubles, but was then relaunched just over a year later on 1st August 2013, with a new direction and change of attitude - becoming a much more laid-back podcast, but with the core values (community participation, etc.) intact.</p>
+
+<p>Much of the success of Steamchat is owed to the contributions and efforts of its listeners, to which the hosts have endless appreciation for.</p>
+
+<h2>History</h2>
+
+<p>Steamchat episodes are produced in audio-only format. Episodes last for around an hour in length. New episodes are released every week, usually on Wednesdays, and are published on <a href="<?php echo $hostLocation; ?>">our website</a>, on two RSS feeds (<a href="<?php echo $hostLocation; ?>/steamchat_feed_mp3.xml">MP3 feed</a> and <a href="<?php echo $hostLocation; ?>/steamchat_feed.xml">M4A feed</a>), on the <a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=320594165">iTunes Store</a> and on our <a href="http://www.youtube.com/steamchat">YouTube channel</a>.</p>
+
+<p>Episodes are structured by opening with discussions about what each of the hosts have recently been playing, then moving on to discussing listener emails and messages, after which (given time) any articles or news topics are discussed. Episodes are usually kept to a family-friendly standard, but every once in a while mature language may be used in either the quoting of others or in discussions.</p>
+
+<h2>Contact</h2>
+
+<p>We love to hear from our listeners, and have always kept our promise to read and discuss every email and message we get on the show. If you would like for us to discuss something on the show, or just want to get in touch, please feel free to contact us using the methods below:</p>
+
+<ul>
+	<li>Email (<a href="mailto:podcast@thesteamchat.com">podcast@thesteamchat.com</a>)</li>
+	<li>Twitter (<a href="http://www.twitter.com/Steamchat">@Steamchat</a>)</li>
+	<li><a href="http://www.facebook.com/Steamchat">Facebook</a></li>
+	<li><a href="http://www.steamcommunity.com/groups/Steamchat">Steam Commuity Group</a></li>
+</ul>
+
+
+<?php require_once "footer.php"; ?>
