7dba88347f2f9b3a461669745550be07b7bd599c
Samuel Kindler <sam@flamov.com>
2015-11-12 11:34:02 +0800

    Updated footer

--- lib/footer.php
+++ lib/footer.php
@@ -1,37 +1,37 @@
 	</main>
 
-	<div id="footer">
+	<footer id="footer">
 
-		<div id="footer_wrapper">
+		<div class="wrapper">
 
-			<a href="<?php echo $hostLocation; ?>" id="footer_logo"><img src="<?php echo $hostLocation; ?>img/footer_logo.png" alt="" /></a>
+			<div class="left">
+				<a href="<?php echo $hostLocation; ?>" id="footer-logo"></a>
+				<ul>
+					<li class="title">Here</li>
+					<li><a href="<?php echo $hostLocation; ?>">Episodes</a></li>
+					<li><a href="<?php echo $hostLocation; ?>specials/">Specials</a></li>
+					<li><a href="<?php echo $hostLocation; ?>about/">About</a></li>
+				</ul>
+				<ul>
+					<li class="title">Elsewhere</li>
+					<li><a href="https://www.twitter.com/thesteamchat">Twitter</a></li>
+					<li><a href="https://www.facebook.com/SteamchatPodcast">Facebook</a></li>
+					<li><a href="https://www.steamcommunity.com/groups/SteamchatPodcast">Steam</a></li>
+					<li><a href="https://www.youtube.com/Steamchat">YouTube</a></li>
+				</ul>
+			</div>
 
-			<ul>
-				<li class="title">Sitemap</li>
-				<li><a href="<?php echo $hostLocation; ?>">Episodes</a></li>
-				<li><a href="<?php echo $hostLocation; ?>specials/">Specials</a></li>
-				<li><a href="<?php echo $hostLocation; ?>about/">About</a></li>
-				<li><span class="tipus_link">Tip Us</span></li>
-				<li><a href="<?php echo $hostLocation; ?>steamchat_feed_mp3.xml">RSS (MP3)</a></li>
-				<li><a href="<?php echo $hostLocation; ?>steamchat_feed.xml">RSS (M4A)</a></li>
-			</ul>
-
-			<ul>
-				<li class="title">Networks</li>
-				<li><a href="http://www.facebook.com/Steamchat">Facebook</a></li>
-				<li><a href="http://www.twitter.com/Steamchat">Twitter</a></li>
-				<li><a href="http://www.steamcommunity.com/groups/Steamchat">Steam</a></li>
-				<li><a href="http://www.youtube.com/Steamchat">YouTube</a></li>
-				<li><a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=320594165">iTunes</a></li>
-			</ul>
-
-			<small>&copy; 2009-2013 Steamchat (The Steamchat, Steamchat Podcast)</small>
-			<small>All Rights Reserved (<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/legalcode">CC BY-NC-ND 3.0</a>)</small>
-			<small>Our listeners are the best</small>
+			<div class="right">
+				<ul>
+					<li>&copy; 2009-2013 Steamchat (The Steamchat, Steamchat Podcast)</li>
+					<li>All Rights Reserved (<a href="http://creativecommons.org/licenses/by-nc-nd/3.0/legalcode">CC BY-NC-ND 3.0</a>)</li>
+					<li>Our listeners are the best!</li>
+				</ul>
+			</div>
 
 		</div>
 
-	</div>
+	</footer>
 
 </body>
 </html>
