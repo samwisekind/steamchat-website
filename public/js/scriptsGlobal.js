8395e973e0bcc6ed3fffd769d0e828ef7fe764f9
Flamov <sam@flamov.com>
2016-12-23 15:01:57 +0800

    #3: Replaced ‘Tip Us!’ with social media icons for desktop layout

--- public/js/scriptsGlobal.js
+++ public/js/scriptsGlobal.js
@@ -1,8 +1,3 @@
-function tipToggle (event) {
-	event.preventDefault();
-	document.getElementsByTagName("body")[0].classList.toggle("headerTip");
-};
-
 function episodeToggle (event) {
 	event.preventDefault();
 	var target = document.getElementById("episodeAudio");
