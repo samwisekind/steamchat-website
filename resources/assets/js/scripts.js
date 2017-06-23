99b7e67f5f43d9db89ca81c413bdf186fe6aae28
Flamov <sam@flamov.com>
2017-06-23 22:51:40 +0800

    Refactored home JavaScript

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,427 +1,250 @@
 var $ = require('jquery');
 
-var $archives;
+(function(){
 
-function bindSidebar() {
+	function bindSidebar() {
 
-	/*** Binding Sidebar ***/
+		var i, targetElement, targetYear, targetCategory;
 
-	var sidebar = $archives.find('.js-sidebar');
+		/*** Binding Sidebar ***/
 
-	$(window).scroll(function() {
+		var sidebar = $archives.find('.js-sidebar');
 
-		if (window.scrollY > ($archives.offset().top - 40)) {
-			sidebar.addClass('sticky');
-		}
-		else {
-			sidebar.removeClass('sticky');
-		}
+		$(window).scroll(function() {
 
-	}).scroll();
-
-
-
-	/*** Binding Search ***/
-
-	var searchField = sidebar.find('.js-search');
-
-	searchField.on('input', function() {
-
-		var value = this.value;
-		var regex = new RegExp(value, 'gi');
-
-		for (i = 0; i < $archives.$episodes.length; i++) {
-
-			var targetElement = $archives.$episodes.eq(i);
-
-			if (targetElement.attr('data-description').search(regex) < 0) {
-				targetElement.addClass('filter-description');
+			if (window.scrollY > ($archives.offset().top - 40)) {
+				sidebar.addClass('sticky');
 			}
 			else {
-				targetElement.removeClass('filter-description');
+				sidebar.removeClass('sticky');
 			}
 
-		}
+		}).scroll();
 
-		if (value !== '') {
-			searchField.addClass('active');
-		}
-		else {
-			searchField.removeClass('active');
-		}
+		/*** Binding Search ***/
 
-		updateFilteringDisplay();
+		var searchField = sidebar.find('.js-search');
 
-	});
+		searchField.on('input', function() {
 
+			var value = this.value;
+			var regex = new RegExp(value, 'gi');
 
+			for (i = 0; i < $archives.$episodes.length; i++) {
 
-	/*** Binding Checkboxes ***/
+				targetElement = $archives.$episodes.eq(i);
 
-	var checkboxes = {
-		elements: sidebar.find('.js-checkbox'),
-		years: {}
-	};
+				if (targetElement.attr('data-description').search(regex) < 0) {
+					targetElement.addClass('filter-description');
+				}
+				else {
+					targetElement.removeClass('filter-description');
+				}
 
-	checkboxes.total = checkboxes.active = checkboxes.elements.length;
+			}
 
-	// Cache the episodes into groups per year by the checkboxes in the sidebar
-	for (var i = 0; i < checkboxes.elements.length; i++) {
-		var targetYear = checkboxes.elements[i].value;
-		checkboxes.years[targetYear] = $archives.$episodes.filter('[data-year="' + targetYear + '"]');
-	}
-
-	checkboxes.elements.on('change', function() {
-
-		// If this is the last checkbox checked, prevent it from becoming unchecked
-		if (this.checked === false && checkboxes.active <= 1) {
-
-			this.checked = true;
-
-		}
-		else {
-
-			this.disabled = false;
-
-			var targetYearGroup = checkboxes.years[this.value];
-
-			if (this.checked === true) {
-				checkboxes.active++;
-				targetYearGroup.removeClass('filter-year');
-				checkboxes.elements.attr('disabled', false);
+			if (value !== '') {
+				searchField.addClass('active');
 			}
 			else {
-				checkboxes.active--;
-				targetYearGroup.addClass('filter-year');
+				searchField.removeClass('active');
 			}
 
-			if (checkboxes.active <= 1){
+			updateFilteringDisplay();
 
-				checkboxes.elements.filter(':checked').attr('disabled', true);
+		});
 
+		/*** Caching episodes **/
+
+		var episodeCache = {
+			inputs: {
+				years: sidebar.find('.js-year'),
+				categories: sidebar.find('.js-category')
+			},
+			episodes: {
+				years: {},
+				categories: {}
+			}
+		};
+
+		// Cache episodes by year
+
+		for (i = 0; i < episodeCache.inputs.years.length; i++) {
+
+			// Get the element
+			targetElement = episodeCache.inputs.years.eq(i);
+
+			// Get the year value
+			targetYear = targetElement.val();
+
+			// Check if the object property already exists
+			if (episodeCache.episodes.years.hasOwnProperty(targetYear) === false) {
+				episodeCache.episodes.years[targetYear] = $archives.$episodes.filter('[data-year="' + targetYear + '"]');
 			}
 
 		}
 
-		updateFilteringDisplay();
+		// Cache episodes by categoryx
 
-	});
+		for (i = 0; i < episodeCache.inputs.categories.length; i++) {
 
+			// Get the element
+			targetElement = episodeCache.inputs.categories.eq(i);
 
+			// Get the category value
+			targetCategory = targetElement.val();
 
-	/*** Episode Count ***/
+			// Check if the object property already exists
+			if (targetCategory !== 'all' && episodeCache.episodes.categories.hasOwnProperty(targetCategory) === false) {
+				episodeCache.episodes.categories[targetCategory] = $archives.$episodes.filter('[data-category="' + targetCategory + '"]');
+			}
 
-	var counter = {
-		element: sidebar.find('.js-count'),
-		total: $archives.$episodes.length,
-		showing: $archives.$episodes.length
-	};
-
-	function updateFilteringDisplay () {
-
-		counter.showing = counter.total - $archives.$episodes.filter('.filter-year, .filter-description').length;
-
-		if (counter.showing < counter.total) {
-			counter.element.html('Showing ' + counter.showing + ' of ' + counter.total + ' episodes');
-			sidebar.addClass('active');
-		}
-		else {
-			sidebar.removeClass('active');
 		}
 
-		if (counter.showing === 0) {
-			$archives.addClass('empty');
+		/*** Binding Year Checkboxes ***/
+
+		var yearsTotal, yearsActive = episodeCache.inputs.years.length;
+
+		episodeCache.inputs.years.on('change', function() {
+
+			// If this is the last checkbox checked, prevent it from becoming unchecked
+			if (this.checked === false && yearsActive <= 1) {
+				this.checked = true;
+			}
+			else {
+
+				this.disabled = false;
+				var selectedYear = episodeCache.episodes.years[this.value];
+
+				if (this.checked === true) {
+					yearsActive++;
+					selectedYear.removeClass('filter-year');
+					episodeCache.inputs.years.attr('disabled', false);
+				}
+				else {
+					yearsActive--;
+					selectedYear.addClass('filter-year');
+				}
+
+				if (yearsActive <= 1) {
+					episodeCache.inputs.years.filter(':checked').attr('disabled', true);
+				}
+
+			}
+
+			updateFilteringDisplay();
+
+		});
+
+		/*** Binding Category Checkboxes ***/
+
+		episodeCache.inputs.categories.on('change', function() {
+
+			var selectedCategory = episodeCache.inputs.categories.filter(':checked').val();
+
+			if (selectedCategory === 'all') {
+				$archives.$episodes.removeClass('filter-category');
+			}
+			else {
+				$archives.$episodes.addClass('filter-category');
+				episodeCache.episodes.categories[selectedCategory].removeClass('filter-category');
+			}
+
+			updateFilteringDisplay();
+
+		});
+
+		/*** Episode Count ***/
+
+		var counter = {
+			element: sidebar.find('.js-count'),
+			total: $archives.$episodes.length,
+			showing: $archives.$episodes.length
+		};
+
+		function updateFilteringDisplay() {
+
+			counter.showing = counter.total - $archives.$episodes.filter('.filter-description, .filter-year, .filter-category').length;
+
+			if (counter.showing < counter.total) {
+				counter.element.html('Showing ' + counter.showing + ' of ' + counter.total + ' episodes');
+				sidebar.addClass('active');
+			}
+			else {
+				sidebar.removeClass('active');
+			}
+
+			if (counter.showing === 0) {
+				$archives.addClass('empty');
+			}
+			else {
+				$archives.removeClass('empty');
+			}
+
 		}
-		else {
-			$archives.removeClass('empty');
-		}
+
+		/*** Binding Reset Button ***/
+
+		$archives.find('.js-reset').on('click', function(event) {
+
+			event.preventDefault();
+
+			// Reset the search field
+			searchField.val('').removeClass('active');
+
+			// Reset the year inputs
+			for (i = 0; i < episodeCache.inputs.years.length; i++) {
+				targetCheckbox = episodeCache.inputs.years.eq(i);
+				targetCheckbox.prop('checked', true).prop('disabled', false);
+			}
+
+			yearsActive = yearsTotal;
+
+			// Reset the category inputs
+			for (i = 0; i < episodeCache.inputs.categories.length; i++) {
+				targetCategory = episodeCache.inputs.categories.eq(i);
+				if (targetCategory.val() === 'all') {
+					targetCategory.prop('checked', true);
+					break;
+				}
+			}
+
+			// Reset the episode elements
+			$archives.$episodes.removeClass('filter-description filter-year filter-category');
+
+			// Update filering display
+			updateFilteringDisplay();
+
+		});
 
 	}
 
-
-
-	/*** Binding Reset Button ***/
-
-	$archives.find('.js-reset').on('click', function(event) {
-
-		event.preventDefault();
-
-		searchField.val('').trigger('input');
-
-		for (var i = 0; i < checkboxes.elements.length; i++) {
-			var targetCheckbox = checkboxes.elements.eq(i);
-			targetCheckbox[0].checked = true;
-			targetCheckbox.trigger('change');
-		}
-
-		checkboxes.active = checkboxes.total;
-
-		updateFilteringDisplay();
-
-	});
-
-}
-
-$(function() {
-
-	$archives = $('.js-archives');
+	var $archives = $('.js-archives');
 
 	if ($archives.length > 0) {
-
 		$archives.$episodes = $archives.find('.js-episode');
 		bindSidebar();
+	}
+
+	var $listen = $('.js-listen');
+
+	if ($listen.length > 0) {
+
+		$listen.on('click', function(event) {
+
+			event.preventDefault();
+
+			var target = document.getElementsByClassName('js-player')[0];
+
+			if (target.paused === true || target.stopped === true) {
+				target.play();
+			}
+			else {
+				target.pause();
+			}
+
+		});
 
 	}
 
-	$('.js-listen').on('click', function(event) {
-
-		event.preventDefault();
-
-		var target = document.getElementsByClassName('js-player')[0];
-
-		if (target.paused === true || target.stopped === true) {
-			target.play();
-		}
-		else {
-			target.pause();
-		}
-
-	});
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var $cacheAudio, $cacheProgress, playerWasPaused, playerUpdateInterval, playerUpdateTimeout, playerPreviousVolume, playerTitleOriginal;
-var playerLoaded = playerBinded = playerHover = false;
-var playerInt = true;
-
-function formatTime (target) {
-
-	target = Number(target);
-	var hours = Math.floor(target / 3600);
-	var minutes = Math.floor(target % 3600 / 60);
-	var seconds = Math.floor(target % 3600 % 60);
-	return (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds
-
-};
-
-function playerBind () {
-
-	playerBinded = true;
-
-	$cachePlayer = $("#headerplayer");
-		$cachePlayer.title = $cachePlayer.find("h1 a");
-		$cachePlayer.date = $cachePlayer.find("h2 a");
-		$cachePlayer.background = $("#header");
-		$cachePlayer.header = $cachePlayer.background.find("#headerMenu");
-	$cacheAudio = $cachePlayer.find("#playerAudio");
-	$cacheVolume = $cachePlayer.find("#playerVolume");
-		$cacheVolume.button = $cacheVolume.find("#playerVolume-toggle");
-		$cacheVolume.slider = $cacheVolume.find("#playerVolume-slider");
-	$cacheProgress = $cachePlayer.find("#playerProgress");
-		$cacheProgress.cover = $cacheProgress.find(".cover");
-		$cacheProgress.line = $cacheProgress.find(".line");
-	$cacheTimestamp = $cachePlayer.find("#playerTime");
-		$cacheTimestamp.current = $cacheTimestamp.find("#playerTime-current");
-		$cacheTimestamp.total = $cacheTimestamp.find("#playerTime-total");
-	$cacheAudio[0].volume = playerLastVolume = 0.8;
-
-	$cacheProgress.mousemove(function (event) {
-		if (playerLoaded == false) {
-			return;
-		};
-		var coordinateX = event.pageX / window.innerWidth;
-		$cacheProgress.line.css("left", ((coordinateX * 100) - .25) + "%");
-		$cacheTimestamp.current.html(formatTime($cacheAudio[0].duration * coordinateX));
-	}).mouseover(function () {
-		playerHover = true;
-	}).mouseout(function () {
-		playerHover = false;
-		playerUpdate();
-	}).on("click", function (event) {
-		if (playerLoaded == false) {
-			return;
-		};
-		$cacheAudio[0].currentTime = $cacheAudio[0].duration * (event.pageX / window.innerWidth);
-		clearTimeout(playerUpdateTimeout);
-		$cacheProgress.cover.addClass("smooth");
-		playerUpdateTimeout = setTimeout(function() {
-			$cacheProgress.cover.removeClass("smooth");
-		}, 250);
-		playerUpdate();
-	});
-
-	$cacheAudio[0].addEventListener("ended", function () {
-		clearInterval(playerUpdateInterval);
-		$cachePlayer.removeClass("playing");
-	});
-
-	$cacheAudio[0].addEventListener("loadedmetadata", function () {
-		$cacheTimestamp.current.html("00:00:00");
-		$cacheTimestamp.total.html(formatTime($cacheAudio[0].duration));
-		$cachePlayer.removeClass("loading");
-		playerLoaded = true;
-		playerUpdate();
-		if (playerInt == true) {
-			$cachePlayer.removeClass("int");
-			playerInt = false;
-			playerToggle(event);
-		};
-		if (playerWasPaused == false) {
-			$cacheAudio[0].play();
-			playerTitleOriginal = document.title;
-			document.title = "Playing: " + $cachePlayer.title.html();
-		};
-	}, false);
-
-	$cacheVolume.slider.on("input change", function () {
-		$cacheAudio[0].volume = parseInt($cacheVolume.slider[0].value) / 100;
-		playerPreviousVolume = $cacheAudio[0].volume;
-		if ($cacheAudio[0].volume == 0) {
-			$cacheVolume.addClass("muted");
-		}
-		else {
-			$cacheVolume.removeClass("muted");
-		};
-	});
-
-};
-
-function playerUpdate () {
-
-	if (playerLoaded == false) {
-		return;
-	};
-
-	if (playerHover == false) {
-		$cacheTimestamp.current.html(formatTime($cacheAudio[0].currentTime));
-	};
-
-	var targetWidth = ($cacheAudio[0].currentTime / $cacheAudio[0].duration) * 100;
-	$cacheProgress.cover.css("width", targetWidth + "%");
-
-	if ($cacheAudio[0].ended == true) {
-		clearInterval(playerUpdateInterval);
-	};
-
-};
-
-function playerToggle (event) {
-
-	event.preventDefault();
-
-	if (playerBinded == false) {
-		playerBind();
-	};
-
-	if (playerInt == true) {
-		$cachePlayer.addClass("loading");
-		$cacheAudio[0].load();
-		return;
-	};
-
-	if ($cacheAudio[0].paused == true) {
-		playerTitleOriginal = document.title;
-		document.title = "Playing: " + $cachePlayer.title.html();
-		$cacheAudio[0].play();
-		playerUpdate();
-		playerUpdateInterval = setInterval(playerUpdate, 1000);
-		$cachePlayer.addClass("playing");
-	}
-	else {
-		document.title = playerTitleOriginal;
-		$cacheAudio[0].pause();
-		clearInterval(playerUpdateInterval);
-		$cachePlayer.removeClass("playing");
-	};
-
-};
-
-function playerChange (event, target) {
-
-	event.preventDefault();
-
-	if (playerBinded == false) {
-		playerBind();
-		playerTitleOriginal = document.title;
-	};
-
-	target = $(target);
-	$cachePlayer.addClass("loading");
-	$("html, body").animate({ scrollTop: 0 }, "slow");
-
-	document.title = playerTitleOriginal;
-	playerWasPaused = $cacheAudio[0].paused;
-	playerLoaded = false;
-	$cachePlayer.title.html(target.parent().find(".title").html());
-	$cachePlayer.date.find("span").html(target.parent().find(".date").html());
-	$cachePlayer.title.add($cachePlayer.date).attr("href", target.parent().find(".link").attr("href"));
-	$cacheProgress.cover.css("width", "0%");
-	$cacheProgress.line.css("left", "0%");
-	if (playerInt == true) {
-		playerInt = false;
-		$cachePlayer.removeClass("int");
-	};
-	$cacheAudio.find("source").attr("src", target.attr("data-audio"));
-	$cacheAudio[0].load();
-	if (target.attr("data-header") != undefined || target.attr("data-background") != undefined) {
-		$cachePlayer.background.removeClass("noBackground");
-		var imageLoadBackground = new Image();
-		imageLoadBackground.src = target.attr("data-background");
-		imageLoadBackground.onload = function() {
-			var imageLoadHeader = new Image();
-			imageLoadHeader.src = target.attr("data-header");
-			imageLoadHeader.onload = function() {
-				$cachePlayer.background.css("background-image", "url(" + imageLoadBackground.src + ")").css("background-color", target.attr("data-color"));
-				$cachePlayer.header.css("background-image", "url(" + imageLoadHeader.src + ")");
-			};
-		};
-	}
-	else {
-		$cachePlayer.background.addClass("noBackground");
-	};
-
-};
-
-function playerMute (event) {
-
-	event.preventDefault();
-
-	if ($cacheAudio[0].volume > 0) {
-		playerPreviousVolume = $cacheVolume.slider[0].value;
-		$cacheAudio[0].volume = $cacheVolume.slider[0].value = 0;
-		$cacheVolume.addClass("muted");
-	}
-	else if (playerPreviousVolume > 0) {
-		$cacheVolume.slider[0].value = playerPreviousVolume;
-		$cacheAudio[0].volume = playerPreviousVolume / 100;
-		$cacheVolume.removeClass("muted");
-	};
-
-};
-
-$(document).ready(function () {
-
-
-
-});
+})();
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -12,21 +12,33 @@
 				<input type="search" placeholder="Filter by keywords..." class="search js-search" />
 
 				<h2>Filter years</h2>
-				<ul class="filter year">
+				<ul class="filter years">
 					@foreach($years as $year)
 						<li>
-							<input type="checkbox" id="year-{{ $year["YEAR(release_date)"] }}" value="{{ $year["YEAR(release_date)"] }}" class="input js-checkbox" checked />
+							<input type="checkbox" id="year-{{ $year["YEAR(release_date)"] }}" value="{{ $year["YEAR(release_date)"] }}" class="input js-year" checked />
 							<label for="year-{{ $year["YEAR(release_date)"] }}" class="label">{{ $year["YEAR(release_date)"] }}</label>
 						</li>
 					@endforeach
 				</ul>
 
 				<h2>Filter type</h2>
-				<ul class="filter types">
-					<li><input type="radio" name="type" id="type-all" value="type-all" class="input js-radio" checked /><label for="type-all" class="label">All</label></li>
-					<li><input type="radio" name="type" id="type-interview" value="type-interview" class="input js-radio" /><label for="type-interview" class="label"><span data-type="interview" class="type interview">Interview</span></label></li>
-		   			<li><input type="radio" name="type" id="type-game" value="type-game" class="input js-radio" /><label for="type-game" class="label"><span data-type="game" class="type game">Game Special</span></label></li>
-		   			<li><input type="radio" name="type" id="type-event" value="type-event" class="input js-radio" /><label for="type-event" class="label"><span data-type="event" class="type event">Event Special</span></label></li>
+				<ul class="filter categories">
+					<li>
+						<input type="radio" name="category" id="category-all" value="all" class="input js-category" checked />
+						<label for="category-all" class="label">All</label>
+					</li>
+					<li>
+						<input type="radio" name="category" id="category-interview" value="interview" class="input js-category" />
+						<label for="category-interview" class="label"><span data-type="interview" class="category interview">Interview</span></label>
+					</li>
+		   			<li>
+		   				<input type="radio" name="category" id="category-game" value="game-special" class="input js-category" />
+		   				<label for="category-game" class="label"><span data-type="game" class="category game">Game Special</span></label>
+		   			</li>
+		   			<li>
+		   				<input type="radio" name="category" id="category-event" value="event-special" class="input js-category" />
+		   				<label for="category-event" class="label"><span data-type="event" class="category event">Event Special</span></label>
+		   			</li>
 				</ul>
 
 			</div>
@@ -40,19 +52,19 @@
 
 			@foreach($episodes as $episode)
 
-				<div class="episode js-episode" data-year="{{ date_format(new DateTime($episode->release_date), 'Y') }}" data-description="{{ $episode->description }}">
-					<a href="#" class="play" data-audio="" onclick="playerChange(event, this);" data-header="" data-background="" data-color=""></a>
+				<div class="episode js-episode" data-description="{{ $episode->description }}" data-year="{{ date_format(new DateTime($episode->release_date), 'Y') }}" data-category="{{ $episode->category }}">
+					<a href="#" class="play" onclick="playerChange(event, this);"></a>
 					<h2><a href="#">{{ $episode->title }}</a></h2>
 					<h3><span>{{ date_format(new DateTime($episode->release_date), 'jS F Y') }}</span> – {{ $episode->file_duration }}</h3>
 					<p>{{ $episode->description }}</p>
-					<ul class="types">
+					<ul class="categories">
 						@isset($episode->category)
-							@if($episode->category === 'inteview')
-								<li><span class="type interview">Interview</span></li>
+							@if($episode->category === 'interview')
+								<li><span class="category interview">Interview</span></li>
 							@elseif($episode->category === 'game-special')
-								<li><span class="type game">Game Special</span></li>
+								<li><span class="category game">Game Special</span></li>
 							@elseif($episode->category === 'event-special')
-								<li><span class="type event">Event Special</span></li>
+								<li><span class="category event">Event Special</span></li>
 							@endif
 						@endisset
 					</ul>
