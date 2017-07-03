2bc0177c738434c95130af2d20e828d96ee753cb
Samuel Kindler <sam@flamov.com>
2017-07-03 15:10:23 +0800

    Updated episode migration with column character limits and comments

--- database/migrations/2017_06_23_080705_create_episodes_table.php
+++ database/migrations/2017_06_23_080705_create_episodes_table.php
@@ -14,22 +14,66 @@ class CreateEpisodesTable extends Migration
     public function up()
     {
         Schema::create('episodes', function (Blueprint $table) {
-            $table->increments('id');
-            $table->boolean('active')->default(false);
-            $table->enum('type', ['episode', 'snack']);
-            $table->integer('number');
-            $table->string('title');
-            $table->string('description');
-            $table->date('release_date');
-            $table->integer('file_size');
-            $table->string('file_url');
-            $table->string('file_duration');
-            $table->string('transcript_url')->nullable()->default(null);
-            $table->enum('category', ['interview', 'game-special', 'event-special'])->nullable()->default(null);
-            $table->string('mask')->nullable()->default(null);
-            $table->string('colour')->nullable()->default(null);
-            $table->string('background')->nullable()->default(null);
-            $table->timestamps();
+
+            $table->increments('id')
+                ->comment('Auto-incrementing ID');
+
+            $table->boolean('active')
+                ->default(false)
+                ->comment('Boolean to show/hide the episode on the website and in data if true/false');
+
+            $table->enum('type', ['episode', 'snack'])
+                ->comment('Type of episode');
+
+            $table->integer('number')
+                ->comment('Episode number respective to its type');
+
+            $table->string('title')
+                ->comment('Title of the episode (without prefixed number or type text)');
+
+            $table->string('description', 255)
+                ->comment('Description of the episode');
+
+            $table->date('release_date')
+                ->comment('Release date of the episode');
+
+            $table->integer('file_size')
+                ->comment('File size of the episode in bytes');
+
+            $table->string('file_url')
+                ->comment('Absolute URL of the episode file');
+
+            $table->string('file_duration')
+                ->comment('Duration of the episode in HH:MM:SS format');
+
+            $table->string('transcript_url')
+                ->nullable()
+                ->default(null)
+                ->comment('Optional absolute URL of the episode transcript location/file');
+
+            $table->enum('category', ['interview', 'game-special', 'event-special'])
+                ->nullable()
+                ->default(null)
+                ->comment('Optional episode category');
+
+            $table->string('mask')
+                ->nullable()
+                ->default(null)
+                ->comment('Optional relative URL (from root of website) of the episode header menu mask image file');
+
+            $table->string('colour')
+                ->nullable()
+                ->default(null)
+                ->comment('Optional episode player background colour (in CSS-valid format)');
+
+            $table->string('background')
+                ->nullable()
+                ->default(null)
+                ->comment('Optional relative URL (from root of website) of the episode player background image file');
+
+            $table->timestamps()
+                ->comment('Creation and modification timestamps');
+
         });
     }
 
