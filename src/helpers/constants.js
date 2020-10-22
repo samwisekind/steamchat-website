bf66b08a760d12705da157c85f5665e7bc7ead50
Flamov <sam@flamov.com>
2020-10-22 11:17:43 +0100

    Remove files

--- src/helpers/constants.js
+++
@@ -1,9 +0,0 @@
-const QUERYFILTER = '-_id -active -createdAt -updatedAt -__v';
-
-const ERRORS = {
-  SERVER_ERROR: 'The server encountered an internal error or misconfiguration and was unable to complete your request.',
-  NOT_FOUND: 'The requested resource was not found, or has been moved or deleted.',
-  INVALID_REQUEST: 'Your request was invalid and could not be completed.',
-};
-
-module.exports = { QUERYFILTER, ERRORS };
